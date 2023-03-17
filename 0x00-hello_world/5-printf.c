6b7c5b158ce55aacd5b007c497c87b96e0c476e4
lawrenceowade <larryowade0@gmail.com>
2023-03-17 12:29:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * This function prints a message to the console and retuens 0 on success
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
