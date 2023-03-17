718cd96f85b19afd796a11a8104ca6721f6e55ca
lawrenceowade <larryowade0@gmail.com>
2023-03-17 12:23:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+ * main - entry point
+ *
+ * This function prints a message to the console and returns 0 on success.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
