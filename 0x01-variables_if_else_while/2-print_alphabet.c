61fb512edc2bbecf53be3247b167d8883127bde6
lawrenceowade <larryowade0@gmail.com>
2023-03-20 02:33:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry pointt
+ *
+ * Description:prints lower case
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+
+{
+int n = 97;
+
+while (n <= 122)
+{
+
+putchar (n);
+n++;
+}
+
+putchar ('\n');
+
+
+
+return (0);
+}
