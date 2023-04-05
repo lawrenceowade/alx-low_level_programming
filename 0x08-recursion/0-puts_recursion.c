c895b038fa54bd7bc97adfa6c302c2a219dcdfba
lawrenceowade <larryowade0@gmail.com>
2023-04-05 09:59:01 -0700

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- *Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
-{	
-if (*s)
 {
-        _putchar(*s);
-	_puts_recursion(s + 1);
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
-
-      else
-
-_putchar('\n');
-}     
+    
