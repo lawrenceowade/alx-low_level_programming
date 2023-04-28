73cfab636485ffe44669adc2a66e28fb54623c21
lawrenceowade <larryowade0@gmail.com>
2023-04-28 11:42:04 -0700

    Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void intro(void) __attribute__ ((constructor));
+
+/**
+ * intro - prints a sentence before the main
+ * function is executed
+ */
+void intro(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
