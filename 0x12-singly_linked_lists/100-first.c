a03ca38f88ac7b1dcbe367368b479340b218f8aa
lawrenceowade <larryowade0@gmail.com>
2023-04-29 08:43:42 -0700

    Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void intro(void) __attribute__ ((constructor));
  */
 void intro(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
