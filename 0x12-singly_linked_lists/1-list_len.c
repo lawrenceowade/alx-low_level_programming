800db8cdb0800f579974c9d44e1fb469ed5bc326
lawrenceowade <larryowade0@gmail.com>
2023-04-29 07:56:04 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,13 @@
  * Return: The number of elements in the linked list.
  */
 
-size_t list_len(const list_t *h) 
+size_t list_len(const list_t *h)
 {
-    size_t len = 0;
-    while (h != NULL) {
-        len++;
-        h = h->next;
-    }
-    return len;
+size_t len = 0;
+while (h != NULL)
+{
+len++;
+h = h->next;
 }
-
-
+return (len);
+}
