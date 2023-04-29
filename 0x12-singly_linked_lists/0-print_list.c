499db6de761b374af78d0806279d0b8b199c82a1
lawrenceowade <larryowade0@gmail.com>
2023-04-28 23:22:27 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,23 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str != NULL)
-            printf("[%d] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
+while (h != NULL)
+{
+if (h->str != NULL)
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
 
-        count++;
-        h = h->next;
-    }
+count++;
+h = h->next;
+}
 
-    return (count);
+return (count);
 }
 
