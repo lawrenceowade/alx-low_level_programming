f2fb9737cb43ad685b087a80f52340fbce7dd405
lawrenceowade <larryowade0@gmail.com>
2023-05-01 01:07:57 -0700

    Get node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a linked list
+ * @head: pointer to the first node of the list
+ * @index: index of the node to return, starting at 0
+ * Return: pointer to the nth node, or NULL if it does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *current = head;
+unsigned int i = 0;
+
+while (current != NULL && i < index)
+{
+current = current->next;
+i++;
+}
+if (i != index)
+{
+return (NULL);
+}
+
+return (current);
+}
+
