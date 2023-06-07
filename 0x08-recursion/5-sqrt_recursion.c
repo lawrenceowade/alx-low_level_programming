0326424d68ad0f676a571cc4a71cc7c1f68d9b75
lawrenceowade <larryowade0@gmail.com>
2023-06-07 05:46:49 -0700

    subconscious

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,9 +23,9 @@ int _sqrt_recursion(int n)
   */
 int _sqrt(int n, int i)
 {
-    if (i * i > n)
-        return (-1);
-    if (i * i == n)
-        return (i);
-    return (_sqrt(n, i + 1));
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt(n, i + 1));
 }
