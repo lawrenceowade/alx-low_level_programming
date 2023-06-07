d862abbb7f223837a2c664e527ab2cfa2b8f3267
lawrenceowade <larryowade0@gmail.com>
2023-06-07 05:00:22 -0700

    idea

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of n raised to the power of m 
+ * @n: value to raise
+ * @m: power
+ *
+ * Return: result of the power
+ */
+int _pow_recursion(int n, int m)
+{
+    return (m < 0) ? -1 : (m == 0) ? 1 : n * _pow_recursion(n, m - 1);
+}
