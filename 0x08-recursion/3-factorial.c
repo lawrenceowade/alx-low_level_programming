fddfe23e93aab0a0a38a2792ceed4c2b6cdbbca1
lawrenceowade <larryowade0@gmail.com>
2023-06-07 04:33:16 -0700

    bigger

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,20 +2,19 @@
 
 /**
  * factorial - returns the factorial of a number
- * @n: number to return the factorial from
+ * @p: number to return the factorial from
  *
  * Return: factorial of p
  */
-int factorial(int p) {
-  
-  if (p < 0) {
-    return (-1);
-  }
-
-  if (p == 0) {
-    return (1);
-  }
- 
-  return (p * factorial(p - 1));
+int factorial(int p)
+{
+if (p < 0)
+{
+return (-1);
 }
-
+if (p == 0)
+{
+return (1);
+}
+return (p * factorial(p - 1));
+}
