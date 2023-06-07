22f1a9645d8b5548feeabb41d9fa2a802726295c
lawrenceowade <larryowade0@gmail.com>
2023-06-07 03:10:32 -0700

    dream

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Prints a string in reverse using recursion.
+ * _print_rev_recursion - Prints a string in reverse using recursion.
  * @s: The string to be printed.
  */
 
@@ -10,7 +10,7 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);  
+		_putchar(*s);
 	}
 }
 
