fccc4cd4cd88d0775558cf893e0d2d0248051cbf
lawrenceowade <larryowade0@gmail.com>
2023-06-11 03:46:48 -0700

    sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,29 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
- /**
+
+/**
   * main - Prints the multiplication of two args numbers
   * @argc: argument count
   * @argv: argument vector
+  *
   * Return: Always zero
   */
-
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
+	int n1 = 0, n2 = 0;
 
-if (argc != 3)
-{
-printf("Error: Please provide two numbers.\n");
-return (1);
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
 }
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 *num2;
-
-printf("Result: %d\n", result);
-
-return (0);
-}
-
